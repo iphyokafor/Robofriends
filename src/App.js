b0404d7dee33a63168d04ce4875870189a20742c
iphyokafor <sandraifeoma22@gmail.com>
2020-04-22 18:48:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => (
-  <div className="tc">
-    <h1>RoboFriends</h1>
-    <SearchBox />
-    <CardList robots={robots} />
-  </div>
-);
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
