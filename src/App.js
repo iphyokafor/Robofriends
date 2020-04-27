203c3caa2545cc373007574ccb0b6bb8870f2c6c
iphyokafor <sandraifeoma22@gmail.com>
2020-04-27 17:02:20 +0100

    Added search component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+/* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots,
+      searchfield: '',
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  }
+
+
   render() {
+    const filteredRobots = this.state.robots.filter((robots) => robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
