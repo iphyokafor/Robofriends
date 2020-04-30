858077d7072818f11d16c4da0b6c7300f953e465
iphyokafor <sandraifeoma22@gmail.com>
2020-04-30 19:15:15 +0100

    [fix]code destructuring

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,19 +28,19 @@ class App extends Component {
 
 
   render() {
-    const filteredRobots = this.state.robots.filter((robots) => robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>;
-    }
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => robot.name.toLowerCase().includes(searchfield.toLowerCase()));
+    return !robots.length
+      ? <h1>Loading</h1>
+      : (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
   }
 }
 export default App;
